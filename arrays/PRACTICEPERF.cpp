3930cf13304cf08e796f1c783c09e3faf65798b9
Priyanshu Chugh <chugh.priyanshu2013@gmail.com>
2022-08-22 01:08:27 +0530

    Second Commit

---
+++ arrays/PRACTICEPERF.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[4];
+    int count = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        if (a[i] >= 10)
+            count++;
+    }
+    cout << count << endl;
+
+    return 0;
+}
