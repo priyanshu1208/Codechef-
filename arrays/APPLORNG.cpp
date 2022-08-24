466f8c0ad47e0e140746a496369891f04087f76a
Priyanshu Chugh <chugh.priyanshu2013@gmail.com>
2022-08-25 00:47:07 +0530

    Questions Added

---
+++ arrays/APPLORNG.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    int a, b;
+    cin >> a >> b;
+    if (a + b <= x)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
